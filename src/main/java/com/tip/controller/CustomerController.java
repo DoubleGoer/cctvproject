8dfe1e4911f6553201a3e157ab2d4a3e92c37f97
DoubleGoer <khs4637@naver.com>
2019-07-05 16:33:20 +0900

    Use Ajax raspberry Pi send data

--- src/main/java/com/tip/controller/CustomerController.java
+++ src/main/java/com/tip/controller/CustomerController.java
@@ -1,5 +1,14 @@
 package com.tip.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpSession;
@@ -7,7 +16,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tip.domain.CaptureDTO;
 import com.tip.domain.MonthDTO;
@@ -64,10 +75,45 @@ public class CustomerController {
 		bb.add(new MonthDTO("금", 40));
 		md.addAttribute("resultList", bb);
 	}
+	
 	@RequestMapping("/controll")
-	public void controll(Model md) {
+	public void controll(Model md,HttpSession se) {
+		ServiceDTO data = new ServiceDTO();
+		data.setC_id((String)se.getAttribute("userid"));
+		ArrayList<ServiceDTO> vo = sc.returnip(data);
+		System.out.println(vo.size());
+		ServiceDTO vs = vo.get(0);
+		md.addAttribute("data", vo);
+		md.addAttribute("first", vs);
 		
-		
+	}
+	@PostMapping("/url.do")
+	public void test(String data,String ip) throws Exception{
+		System.out.println(data);
+		System.out.println(ip);
+		try{
+            String ip1 = ip; //클라이언트의 임시 접속 주소
+            int port = 9000; //접속할 서버 포트
+            Socket socket = new Socket(ip1, port); //클라이언트의 소켓 생성
+         
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+ 
+            OutputStream out = socket.getOutputStream(); //서버의 소켓으로부터 출력을 받음
+            InputStream in = socket.getInputStream(); //서버의 소켓으로부터 입력을 받음
+             
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(out)); //출력 스트림을 변환
+            BufferedReader br = new BufferedReader(new InputStreamReader(in)); //입력 스트림을 변환
+             
+            pw.println("r");
+
+            pw.println("q");
+            pw.close();
+            br.close();
+            socket.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
 	}
 
 }
